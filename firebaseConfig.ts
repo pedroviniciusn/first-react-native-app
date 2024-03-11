a8a5de47e50d1b8df059074e9c19c579bb39d18d
pedroviniciusn <pedrovinicius.dev@gmail.com>
2024-03-10 21:45:13 -0300

    feat: add firebase

---
+++ firebaseConfig.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCGUSIURlkkKotLEi8WQ2fN8UryEqqGqu4',
+  authDomain: 'dev-house-58e7e.firebaseapp.com',
+  projectId: 'dev-house-58e7e',
+  storageBucket: 'dev-house-58e7e.appspot.com',
+  messagingSenderId: '400032454768',
+  appId: '1:400032454768:web:5ac650629012c2b1f93cb5',
+  measurementId: 'G-Z2GBE18RER',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
