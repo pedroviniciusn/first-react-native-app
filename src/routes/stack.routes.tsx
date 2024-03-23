2ef4c821035abc2e7f50f6da0f05a94479eaaadc
pedroviniciusn <pedrovinicius.dev@gmail.com>
2024-03-22 22:09:45 -0300

    feat: method login redirect home screen

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,20 +1,25 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../screens/Login';
 import SignUp from '../screens/SignUp';
+import Home from '../screens/Home';
 
 export type AppStackParamList = {
   Login: undefined;
   SignUp: undefined;
+  Home: undefined;
 };
 const Stack = createNativeStackNavigator<AppStackParamList>();
 
 const StackRoutes = () => {
   return (
-    <Stack.Navigator screenOptions={{
-      headerShown: false
-    }}>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   );
 };
